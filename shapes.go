5be0b5d404c8c3da35edb8308e9d792f292250f1
Tim Becker <tim.becker@kuriositaet.de>
2013-06-11 23:57:47 +0200

    initial shapes

---
+++ shapes.go
@@ -0,0 +1,245 @@
+package shapefile
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+var L = binary.LittleEndian
+var B = binary.BigEndian
+
+func readType(r io.Reader, t_expected ShapeType) error {
+	var t ShapeType
+	binary.Read(r, binary.LittleEndian, t)
+	if t != t_expected {
+		return fmt.Errorf("unexpected type: %d expected: %d", t, t_expected)
+	}
+	return nil
+}
+
+type Null struct {
+}
+
+func ReadNull(r io.Reader) (n *Null, err error) {
+	err = readType(r, NULL_SHAPE)
+	return &Null{}, nil
+}
+
+type Point struct {
+	_ ShapeType // Point is an embedded type
+	X float64
+	Y float64
+}
+
+func ReadPoint(r io.Reader) (p *Point, err error) {
+	//err= readType(r, POINT)
+	p = &Point{}
+	err = binary.Read(r, binary.LittleEndian, p)
+	return
+}
+
+type Box struct {
+	Xmin float64
+	Ymin float64
+	Xmax float64
+	Ymax float64
+}
+type MultiPoint struct {
+	Box       Box
+	NumPoints int32
+	Points    []*Point
+}
+
+func ReadMultiPoint(r io.Reader) (mp *MultiPoint, err error) {
+	if err = readType(r, MULTI_POINT); err != nil {
+		return
+	}
+	mp = &MultiPoint{}
+	if err = binary.Read(r, binary.LittleEndian, &mp.Box); err != nil {
+		return
+	}
+	if err = binary.Read(r, binary.LittleEndian, &mp.NumPoints); err != nil {
+		return
+	}
+
+	mp.Points = make([]*Point, mp.NumPoints)
+	if err = binary.Read(r, binary.LittleEndian, mp.Points); err != nil {
+		return
+	}
+
+	return
+}
+
+type PolyLine struct {
+	Box       Box
+	NumParts  int32
+	NumPoints int32
+	Parts     []int32
+	Points    []*Point
+}
+
+func ReadPolyLine(r io.Reader) (pl *PolyLine, err error) {
+	if err = readType(r, POLY_LINE); err != nil {
+		return
+	}
+	pl = &PolyLine{}
+	if err = binary.Read(r, L, &pl.Box); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &pl.NumParts); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &pl.NumPoints); err != nil {
+		return
+	}
+	pl.Parts = make([]int32, pl.NumParts)
+	if err = binary.Read(r, L, &pl.Parts); err != nil {
+		return
+	}
+	pl.Points = make([]*Point, pl.NumPoints)
+
+	err = binary.Read(r, L, &pl.Parts)
+
+	return
+
+}
+
+type Polygon struct {
+	PolyLine
+}
+
+func ReadPolygon(r io.Reader) (pg *Polygon, err error) {
+	if err = readType(r, POLYGON); err != nil {
+		return
+	}
+	pg = &Polygon{}
+	if err = binary.Read(r, L, &pg.Box); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &pg.NumParts); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &pg.NumPoints); err != nil {
+		return
+	}
+	pg.Parts = make([]int32, pg.NumParts)
+	if err = binary.Read(r, L, &pg.Parts); err != nil {
+		return
+	}
+	pg.Points = make([]*Point, pg.NumPoints)
+
+	err = binary.Read(r, L, &pg.Parts)
+
+	return
+
+}
+
+type PointM struct {
+	X float64
+	Y float64
+	M float64
+}
+
+func ReadPointM(r io.Reader) (pm *PointM, err error) {
+	if err = readType(r, POINT_M); err != nil {
+		return
+	}
+	pm = &PointM{}
+	err = binary.Read(r, L, pm)
+	return
+}
+
+type MRange struct {
+	Mmin float64
+	Mmax float64
+}
+type MultiPointM struct {
+	Box       Box
+	NumPoints int32
+	Points    []*Point
+	MRange    MRange
+	MArray    []float64
+}
+
+func ReadMultiPointM(r io.Reader) (mp *MultiPointM, err error) {
+	if err = readType(r, MULTI_POINT_M); err != nil {
+		return
+	}
+	mp = &MultiPointM{}
+	if err = binary.Read(r, L, &mp.Box); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &mp.NumPoints); err != nil {
+		return
+	}
+	mp.Points = make([]*Point, mp.NumPoints)
+	if err = binary.Read(r, L, mp.Points); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &mp.MRange); err != nil {
+		return
+	}
+	mp.MArray = make([]float64, mp.NumPoints)
+	err = binary.Read(r, L, mp.MArray)
+	return
+
+}
+
+type PolyLineM struct {
+	Box       Box
+	NumParts  int32
+	NumPoints int32
+	Parts     []int32
+	Points    []*Point
+	MRange    MRange
+	MArray    []float64
+}
+
+func ReadPolyLineM(r io.Reader) (pl *PolyLineM, err error) {
+	if err = readType(r, MULTI_POINT_M); err != nil {
+		return
+	}
+	pl = &PolyLineM{}
+	if err = binary.Read(r, L, pl.Box); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &pl.NumParts); err != nil {
+		return
+	}
+	if err = binary.Read(r, L, &pl.NumPoints); err != nil {
+		return
+	}
+	pl.Parts = make([]int32, pl.NumParts)
+	if err = binary.Read(r, L, pl.Parts); err != nil {
+		return
+	}
+	var point *Point
+	for i := (int32)(0); i != pl.NumPoints; i++ {
+		if point, err = ReadPoint(r); err != nil {
+			return
+		}
+		pl.Points = append(pl.Points, point)
+	}
+	if err = binary.Read(r, L, pl.MRange); err != nil {
+		return
+	}
+	pl.MArray = make([]float64, pl.NumPoints)
+
+	err = binary.Read(r, L, pl.MArray)
+	return
+}
+
+type PolygonM struct {
+	PolyLineM
+}
+type PointZ struct {
+}
+type MultiPointZ struct {
+}
+type PolyLineZ struct {
+}
+type PolygonZ struct {
+}
+type MultiPatch struct {
+}
